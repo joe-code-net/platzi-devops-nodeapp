a7d6cc4b1e9619aee6e39c72af84655a3dad74d0
“joe.code.net” <joe.code.net@gmail.com>
2018-02-20 23:32:03 -0700

    Update state

--- infra/main.tf
+++ infra/main.tf
@@ -24,13 +24,23 @@ resource "digitalocean_tag" "platzi-devops-05" {
 }
 
 resource "digitalocean_droplet" "platzi-devops-05" {
-  count  = 1
-  image  = "31640252"
+  count  = 2
+  image  = "${var.image_id}"
   name   = "platzi-devops-05"
   region = "sfo2"
   size   = "s-1vcpu-1gb"
   ssh_keys = [18198313]
   tags = ["${digitalocean_tag.platzi-devops-05.id}"]
+  
+  lifecycle
+  {
+    create_before_destroy = true
+  }
+  
+  provisioner "local-exec" {
+    command = "sleep 160 && curl ${self.ipv4_address}:3000"
+  }
+
   user_data = <<EOF
 #cloud-config
 coreos:
